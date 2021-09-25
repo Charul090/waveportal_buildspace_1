5031801c3cb8c9ba1710949e87a0e5ea460d499d
Charul Bari <34866039+Charul090@users.noreply.github.com>
2021-09-25 22:00:37 +0000

    Added metamask connection functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
-import * as React from "react";
+import React,{  useState, useEffect } from "react";
 import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
+  const [ address, setAddress] = useState('');
+
+  const checkWalletConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log('Wallet Not Connected');
+      return;
+    }
+    console.log('Wallet Connected');
+
+    try {
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length > 0) {
+        setAddress(accounts[0])
+      }
+    
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    checkWalletConnected();
+  }, []);
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert('Download Metamask');
+      }
+
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts'
+      });
+
+      console.log('Current Account', accounts[0]);
+      setAddress(accounts[0]);
+
+    } catch (e) {
+      console.log('Error while connecting wallet', e);
+    }
+  }
 
   const wave = () => {
     
@@ -23,6 +68,12 @@ export default function App() {
         <button className="waveButton" onClick={wave}>
           Wish me luck 👍
         </button>
+        {
+          !address && 
+          <button onClick={connectWallet} className="waveButton">
+            Connect with MetaMask!
+          </button>
+        }
       </div>
     </div>
   );
