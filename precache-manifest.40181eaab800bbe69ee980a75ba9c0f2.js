fd278b28d37cff4a2c84c676a97b4c9ffebeb1f4
Charul090 <baricharul9@hotmail.com>
2021-09-26 14:12:45 +0530

    Updates

---
+++ precache-manifest.40181eaab800bbe69ee980a75ba9c0f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2646ba24ce54c7ec3ec98d62175fd5",
+    "url": "/waveportal_buildspace_1/index.html"
+  },
+  {
+    "revision": "8e2f233db86190974abe",
+    "url": "/waveportal_buildspace_1/static/css/main.0e632e19.chunk.css"
+  },
+  {
+    "revision": "a6ead7aec961e4f6a1b8",
+    "url": "/waveportal_buildspace_1/static/js/2.3db25c41.chunk.js"
+  },
+  {
+    "revision": "8e2f233db86190974abe",
+    "url": "/waveportal_buildspace_1/static/js/main.32e96652.chunk.js"
+  },
+  {
+    "revision": "ea962534537056087925",
+    "url": "/waveportal_buildspace_1/static/js/runtime-main.acadd2c2.js"
+  }
+]);
