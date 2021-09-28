a0017f80a76acbcdc4a44e03e4ae51a3faffef4c
Charul090 <baricharul9@hotmail.com>
2021-09-28 07:55:51 +0530

    Added texfield and table

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React,{  useState, useEffect } from "react";
 import { ethers } from "ethers";
 import './App.css';
 import contract from './constants/contract.js';
-import waveportal from './utils/waveportal.json'
+import waveportal from './utils/waveportal.json';
+import MaterialTable from "material-table";
 
 export default function App() {
   const [ address, setAddress] = useState('');
   const [ wished, setWellWishers ] = useState(0);
+  const [ wavesList, setWavesList ] = useState([]);
+  const [ message, setMessage ] = useState('');
 
   const checkWalletConnected = async () => {
     const { ethereum } = window;
@@ -34,10 +37,6 @@ export default function App() {
       console.log(e)
     }
   }
-
-  useEffect(async () => {
-    checkWalletConnected();
-  }, []);
 
   const connectWallet = async () => {
     try {
@@ -68,15 +67,16 @@ export default function App() {
         const waveportalContract = new ethers.Contract(contract.waves, waveportal.abi, signer);
 
         await waveportalContract.getTotalWaves();
-
-        const waveTxn = await waveportalContract.wave();
+        const waveMessage = message || 'N.A';
+        const waveTxn = await waveportalContract.wave(waveMessage);
         await waveTxn.wait();
 
         const count = await waveportalContract.getTotalWaves();
 
         console.log(count.toNumber());
         setWellWishers(count.toNumber());
-        
+        setMessage('');
+        getAllWaves();
       } else {
         console.log('No wallet connected')
       }
@@ -87,34 +87,94 @@ export default function App() {
       console.log(e);
     }
   }
+
+  const getAllWaves = async () => {
+    const { ethereum } = window;
+    try {
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const waveportalContract = new ethers.Contract(contract.waves, waveportal.abi, signer);
+  
+        const waves = await waveportalContract.getAllWaves();
+        const wavesList = waves.map((waveInfo) => {
+          return {
+            address: waveInfo.waver,
+            timestamp: new Date(waveInfo.timestamp * 1000).toLocaleString(),
+            message: waveInfo.name
+          }
+        });
+  
+        setWavesList(wavesList);
+      } else {
+        console.log('Metamask not connected');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    const asyncFn = async () => {
+      await checkWalletConnected();
+      await getAllWaves();
+    }
+
+    asyncFn();
+  }, []);
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  }
   
   return (
-    <div className="mainContainer">
+    <React.Fragment>
 
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
-        <div className="bio">
-        I am Charul and this is my first project in Blockchain. Wish me luck!
-        </div>
-        {
-          address &&
-          <div className="bio">
-          No of well wishers: {wished || ''} 
+      <div className="mainContainer">
+        <div className="dataContainer">
+          <div className="header">
+          👋 Hey there!
           </div>
-        }
-
-        <button className="waveButton" onClick={wave} disabled={!address}>
-          Wish me luck 👍
-        </button>
-        {
-          !address && 
-          <button onClick={connectWallet} className="waveButton">
-            Connect with MetaMask!
+          <div className="bio">
+          I am Charul and this is my first project in Blockchain. Wish me luck!
+          </div>
+          {
+            address &&
+            <div className="bio">
+            No of well wishers: {wished || ''} 
+            </div>
+          }
+          <input type='text' value={message} onChange={handleChange} placeholder='Type the message' className="input-field"/>
+          <button className="waveButton" onClick={wave} disabled={!address}>
+            Wish me luck <span>👍</span>
           </button>
-        }
+          {
+            !address && 
+            <button onClick={connectWallet} className="waveButton">
+              Connect with MetaMask!
+            </button>
+          }
+        </div>
       </div>
-    </div>
+      {
+        address && wavesList.length > 0 &&
+        <div className='list'>
+          <MaterialTable
+            columns={[
+              { title: 'Address', field: 'address' },
+              { title: 'Message', field: 'message' },
+              { title: 'Time', field: 'timestamp' }
+            ]}
+            data={wavesList}
+            options={
+              {
+                showTitle: false,
+                toolbar: false
+              }
+            }
+          />
+        </div> 
+      }
+    </React.Fragment>
   );
 }
